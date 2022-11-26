57dd578b57817a6c0450d23b2ae5d3d974ac3c27
Sinthoras39 <digc0820@gmail.com>
2022-11-26 11:04:35 +0100

    initial commit

---
+++ src/lib.rs
@@ -0,0 +1,29 @@
+use std::io::{stdin, stdout, Write};
+use std::str::FromStr;
+
+
+fn get_str(prompt: &str) -> String {
+    print!("{}", prompt);
+    let _ = stdout().flush().expect("Console flush failed.");
+
+    let mut buffer:String = String::new();
+
+    // `read_line` returns `Result` of bytes read
+    let _ = stdin().read_line(&mut buffer).expect("Reading line failed.");
+    return buffer.trim().to_string();
+}
+
+pub fn get<T: FromStr>(prompt: &str, err_promt: &str) -> T {
+    let i: String = get_str(prompt);
+    let res = i.parse::<T>();
+    let c;
+    match res {
+        Ok(a) => c = a,
+        Err(_) =>{
+            println!("{}", err_promt);
+            c = get::<T>(prompt, err_promt);
+        },
+    }
+    return c;
+}
+
